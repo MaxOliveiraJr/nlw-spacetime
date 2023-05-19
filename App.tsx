7ff305be51efb6a4f5255a8f18e15c430f8b50a2
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-19 19:18:56 -0300

    background concluido

--- App.tsx
+++ App.tsx
@@ -1,26 +1,37 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, View } from 'react-native'
 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
+import bgBlur from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+const StyledStripes = styled(Stripes)
 
 export default function App() {
-
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
-    BaiJamjuree_700Bold
+    BaiJamjuree_700Bold,
   })
 
-  if(!hasLoadedFonts){
+  if (!hasLoadedFonts) {
     return null
   }
 
   return (
-    <View className="bg-gray font-sans">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={bgBlur}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2"></StyledStripes>
+      <StatusBar style="light" translucent></StatusBar>
+    </ImageBackground>
   )
 }
